aef2b75fcb62e819fac579da06d91749abadd713
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-06-23 16:38:53 +0530

    Create control scene to view key bindings

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,3 +9,7 @@ func _on_start_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/controls.tscn")
